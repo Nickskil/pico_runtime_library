7aa0a484d9f4720a36f48501eb985f0b0c6e87c0
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2026-01-10 21:33:14 +0100

    Fix: Adapt flexible definition of default LED for different boards

--- hal/led/led.c
+++ hal/led/led.c
@@ -1,47 +1,77 @@
 #include "hal/led/led.h"
 #include "hardware/gpio.h"
-#ifdef PICO_CYW43_SUPPORTED
+#ifdef LED_CYW43_SUPPORTED
     #include "pico/cyw43_arch.h"
+#elif LED_WS2812_SUPPORTED
+	#include "peri/ws2812/ws2812.h"
 #endif
 
 // ======================================== INTERNAL FUNCTIONS ===============================================
 uint8_t pin_used = 0;
+bool led_state = false;
 
 
 // ======================================== CALLABLE FUNCTIONS ===============================================
-void init_default_led(uint8_t led_pin){
-	#ifdef PICO_CYW43_SUPPORTED
-		cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
+void set_gpio_default_led(uint8_t led_pin){
+	pin_used = led_pin;
+};
+
+
+uint8_t get_gpio_default_led(void){
+	return pin_used;
+};
+
+
+void init_default_led(void){
+	led_state = false;
+	#ifdef LED_CYW43_SUPPORTED
+		if (cyw43_arch_init()) {
+            return false;
+        }
 		pin_used = CYW43_WL_GPIO_LED_PIN;
+		cyw43_arch_gpio_put(pin_used, led_state);
+	#elif LED_WS2812_SUPPORTED
+		pin_used = 17;
+		PIO  pio = pio0;
+		ws2812_init(pio, pin_used);
+		put_pixel_rgb(0, 0, 0); 		
 	#else
-		pin_used = led_pin;
 		gpio_init(pin_used);
 		gpio_set_dir(pin_used, GPIO_OUT);
-		gpio_put(pin_used, false);
+		gpio_put(pin_used, led_state);
 	#endif
 };
 
 
-bool set_default_led(bool state){
-	#ifdef PICO_CYW43_SUPPORTED 
+bool set_state_default_led(bool state){
+	led_state = state;
+	#ifdef LED_CYW43_SUPPORTED 
 		cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, state);
+	#elif LED_WS2812_SUPPORTED
+		if(state) 
+			put_pixel_rgb(0, 32, 0);
+		else
+			put_pixel_rgb(0, 0, 0);
 	#else
 		gpio_put(pin_used, state);
 	#endif
-	return state;
+	return led_state;
 };
 
 
-bool get_default_led(void){
-	#ifdef PICO_CYW43_SUPPORTED
-		return cyw43_arch_gpio_get(CYW43_WL_GPIO_LED_PIN);
+bool get_state_default_led(void){
+	#ifdef LED_CYW43_SUPPORTED
+		led_state = cyw43_arch_gpio_get(CYW43_WL_GPIO_LED_PIN);
+	#elif LED_WS2812_SUPPORTED
+		led_state = led_state;
 	#else
-		return gpio_get(pin_used);
+		led_state = gpio_get(pin_used);
 	#endif
+	return led_state;
 };
 
 
-bool toggle_default_led(void){
-	set_default_led(!get_default_led());
-	return get_default_led();
+bool toggle_state_default_led(void){
+	set_state_default_led(!get_state_default_led());
+	return get_state_default_led();
 };
--- hal/led/led.h
+++ hal/led/led.h
@@ -6,30 +6,42 @@
 
 
 // ============================== INTERNAL FUNCTIONS =========================================
-/*! Function for initializing the default LED.
+/*! Function for setting the GPIO pin number of the default LED.
  * @param led_pin: The GPIO pin number for the LED (if not cyw43 LED is used).
  * @return None
  */
-void init_default_led(uint8_t led_pin);
+void set_gpio_default_led(uint8_t led_pin);
+
+
+/*! Function for getting the GPIO pin number of the default LED.
+ * @return The GPIO pin number for the LED (if not cyw43 LED is used).
+ */
+uint8_t get_gpio_default_led(void);
+
+
+/*! Function for initializing the default LED.
+ * @return None
+ */
+void init_default_led(void);
 
 
 /*! Function for setting the state of the default LED.
  * @param state: The state to set the LED to (true for on, false for off).
  * @return       The state of the LED after setting it.
  */
-bool set_default_led(bool state);
+bool set_state_default_led(bool state);
 
 
 /*! Function for getting the state of the default LED.
  * @return          The actual state of the LED.
  */
-bool get_default_led(void);
+bool get_state_default_led(void);
 
 
 /*! Function for toggling the state of the default LED.
  * @return          The actual state of the LED.
  */
-bool toggle_default_led(void);
+bool toggle_state_default_led(void);
 
 
 #endif
